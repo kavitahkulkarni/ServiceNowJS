68f6ec3f58276925406a82e050c9a50412eaf108
Kavita Kulkarni <kavitahkulkarni@users.noreply.github.com>
2020-08-23 15:21:32 -0700

    Create recursion.js

---
+++ recursion.js
@@ -0,0 +1,18 @@
+function factorial(x) {
+
+  // TERMINATION
+  if (x < 0)
+    return;
+
+  // BASE
+  if (x === 0)
+    return 1;
+
+  // RECURSION
+  return x * factorial(x - 1);
+}
+gs.info(factorial(3)); // 6
+
+
+
+// *** Script: 6
